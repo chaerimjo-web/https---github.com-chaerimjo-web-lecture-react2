7d285e904625a2bf02ecc919942a2c7b544e1a6e
chaerimjo-web <chaerim010710@gmail.com>
2025-06-24 11:06:10 +0900

    설명 추가

--- node_modules/shared/lib/EventEmitter.js
+++ node_modules/shared/lib/EventEmitter.js
@@ -1,3 +1,6 @@
+/* **이벤트 발생기(Event Emitter)**를 만드는 팩토리 함수 */
+/* createEventEmitter는 내부 상태 값(value)을 관리하면서, 이 값이 변경될 때 등록된 핸들러(콜백 함수)들을 실행해주는 구조 */
+
 const createEventEmitter = (value) => {
   let handlers = [];
 
